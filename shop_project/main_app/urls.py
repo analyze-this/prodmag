d83dc8742e893a0ea61e6dad8319f40f2a79fdf6
analyze-this <barristers@tut.by>
2021-09-21 21:34:54 +0300

    21.09.2021

--- shop_project/main_app/urls.py
+++ shop_project/main_app/urls.py
@@ -5,6 +5,7 @@ from .views import *
 app_name = 'main_app'
 urlpatterns = [
     path('', MainWithCategory.as_view(), name='supermain'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='productdetail'),
     path('<int:category_id>/', MainWithCategory2.as_view(), name='supermain1'),
     path('main/', main_view, name='main'),
     path('view/', ClassView.as_view(), name='view'),
